bdd26f8d943c8e6e845945743615097810376451
mehdiMajedi20 <workmajedi@gmial.com>
2023-08-11 16:55:55 +0330

    base project todo list

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+import { AnyAction } from '@reduxjs/toolkit';
+
+export interface IModel {
+  id: string;
+  text: string;
+  title?:string;
+  isFinished: boolean;
+  createdAt?: string;
+  updatedAt?: string;
+  isTextShowed?: boolean;
+}
+
+export type TActionSlice = Omit<IModel, 'text'>;
+export type TUpdateTextShowed = Omit<TActionSlice, 'isFinished'>;
+
+export interface IColumnLayoutProps {
+  labelText?: string;
+  addHandler: (title:string ,v: string ) => AnyAction;
+  removeHandler: (v: string) => AnyAction;
+  completedHandler: (v: TActionSlice) => AnyAction;
+  selectorState: IModel[];
+  droppableId: string;
+  updateTextShowed: (v: TUpdateTextShowed) => AnyAction;
+}
